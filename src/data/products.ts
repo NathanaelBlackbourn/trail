b658b8033a1472e3eb243da46f8200d6d24236b4
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2023-08-06 12:51:00 +0200

    Feat: Inventory data file written, empty Footer.tsx added.

---
+++ src/data/products.ts
@@ -0,0 +1,56 @@
+export const inventory = [
+  {
+    categoryName: "Hike",
+    id: 1,
+    products: [
+      {
+        id: 1,
+        name: "Andreas Strap",
+        brand: "ROA",
+        price: 3500,
+        availableSizes: ["36", "37", "38", "40", "41", "42", "43", "44", "45"],
+      },
+      {
+        id: 2,
+        name: "Cingino",
+        brand: "ROA",
+        price: 3500,
+        availableSizes: ["36", "37", "38", "39", "40", "41", "43", "45"],
+      },
+      {
+        id: 3,
+        name: "Sandal",
+        brand: "ROA",
+        price: 3900,
+        availableSizes: ["36", "37", "39", "40", "41", "42,", "43", "45"],
+      },
+    ],
+  },
+  {
+    categoryName: "Run",
+    id: 2,
+    products: [
+      {
+        id: 4,
+        name: "XA pro 3d V9",
+        brand: "Salomon",
+        price: 1695,
+        availableSizes: ["36", "37", "38", "40", "41", "42", "43", "44", "45"],
+      },
+      {
+        id: 5,
+        name: "Pulsar Trail Pro 2",
+        brand: "Salomon",
+        price: 1295,
+        availableSizes: ["36", "39", "40", "41", "43", "45"],
+      },
+      {
+        id: 6,
+        name: "Ultra Glide 2 Trail Paris",
+        brand: "Salomon",
+        price: 1440,
+        availableSizes: ["36", "37", "41", "42,", "43", "45"],
+      },
+    ],
+  },
+];
