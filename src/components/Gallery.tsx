b7bcbba408c3cb2275c9785d2b174b70629d4014
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2023-08-06 12:21:33 +0200

    Feat: Cropped gallery image added.

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -3,7 +3,7 @@ import styles from "@/styles/Gallery.module.scss";
 export default function Gallery() {
   return (
     <div className={styles.container}>
-      <img src="/images/Cingino.webp" alt="ROA Cingino shoe" />
+      <img src="/images/Cingino-gallery.webp" alt="ROA Cingino shoe" />
     </div>
   );
 }
