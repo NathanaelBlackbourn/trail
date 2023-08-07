9907ac2ad0b008b427610ca1876ce55ae60e83d9
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2023-08-07 12:37:13 +0200

    Feat: Interlude structure implemented.

--- src/components/Interlude.tsx
+++ src/components/Interlude.tsx
@@ -1,3 +1,17 @@
+import styles from "@/styles/Interlude.module.scss";
+
 export default function Interlude() {
-  return <></>;
+  return (
+    <div className={`row ${styles.container}`}>
+      <div className={`column ${styles.left}`}>
+        <h2>ALL PRODUCTS MUST MEET OUR SUSTAINABILITY STANDARDS</h2>
+      </div>
+      <div className={`column ${styles.right}`}>
+        <h4>Read more about our environmental goals</h4>
+        <div className={styles.arrow}>
+          <svg></svg>
+        </div>
+      </div>
+    </div>
+  );
 }
