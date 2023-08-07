a7b38a81c43075af27fec345af5b0ff680883401
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2023-08-07 11:55:13 +0200

    Feat: Products rendering as in mockup.

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -8,10 +8,18 @@ export default function Product({ product }: Props) {
   return (
     <div className={`column ${styles.container}`}>
       <div className={styles.imgContainer}>
-        <img src={product.imgurl} alt={`Image of ${product.name}`}></img>
+        <img
+          src={product.imgurl}
+          alt={`Image of ${product.name}`}
+          className={styles.image}
+        ></img>
+        <h5 className={styles.brand}>{product.brand}</h5>
+        <div className={styles.sizeWrapper}>
+          <p>+</p>
+        </div>
       </div>
       <p>{product.name}</p>
-      <p>{product.price}</p>
+      <p>{product.price} SEK</p>
     </div>
   );
 }
