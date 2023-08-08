808522b0496684ff3f312a07b6ff66314068e7c8
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2023-08-08 13:29:25 +0200

    Feat: Draft Menu component written.

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,3 +1,27 @@
+import Header from "../Header/Header";
+import styles from "./Menu.module.scss";
+
 export default function Menu() {
-  return <></>;
+  return (
+    <div className={styles.container}>
+      <div className={`row ${styles.top}`}>
+        <div className={"column"}></div>
+        <div className={"column"}>
+          <h4>Product Care</h4>
+          <h4>Order & Shipping</h4>
+          <h4>Return Policy</h4>
+          <h4>Terms & Coniditions</h4>
+          <h4>Privacy Policy</h4>
+          <h4>Cookie Policy</h4>
+          <h4>Advertising Cookies</h4>
+          <h4>Preferences</h4>
+        </div>
+        <div className={"column"}>
+          <p>+46 712345678</p>
+          <p>email@trail.eu</p>
+        </div>
+      </div>
+      <Header />
+    </div>
+  );
 }
