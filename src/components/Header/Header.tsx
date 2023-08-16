27fe31c904710407583e2b8383488c1175feda95
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2023-08-16 13:35:55 +0200

    Feat: Unstable header animation using requestAnimationFrame and delta time.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import { useMenuContext } from "@/contexts/MenuContext";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import LinkWrapper from "../LinkWrapper/LinkWrapper";
 import styles from "./Header.module.scss";
 
@@ -10,42 +10,66 @@ interface Props {
 export default function Header({ variant }: Props) {
   const titleRef = useRef<HTMLHeadingElement>(null);
   const iconRef = useRef<HTMLDivElement>(null);
-
-  const [stretch, setStretch] = useState(0);
+  const deltaTimeTracker = useRef(performance.now());
+  const stretchTracker = useRef(500);
+  const [stretch, setStretch] = useState<number>(stretchTracker.current);
 
   const { menuOpen, setMenuOpen } = useMenuContext();
+  const debounceTimer = useRef<NodeJS.Timeout>();
 
-  let debounceTimer: NodeJS.Timeout;
+  const regulateStretch = useCallback(() => {
+    stretch < 500 && setStretch(500);
+    stretch > 1000 && setStretch(1000);
+  }, [stretch]);
 
-  const debounceStretch = () => {
-    clearTimeout(debounceTimer);
-    debounceTimer = setTimeout(() => {
-      stretchTitle();
+  const getDiff = useCallback(() => {
+    if (!iconRef.current || !titleRef.current) return;
+    const iconWidth = iconRef.current.getBoundingClientRect().width;
+    const titleWidth = titleRef.current.getBoundingClientRect().width;
+    const space = window.innerWidth - iconWidth;
+    return space - titleWidth;
+  }, []);
+  const stretchTitle = useCallback(
+    (timeStamp: DOMHighResTimeStamp) => {
+      requestAnimationFrame(stretchTitle);
+
+      // regulateStretch();
+
+      const diff = getDiff();
+      if (!diff) return;
+
+      const adjustStretch = (diff: number) => {
+        console.log(diff);
+        diff <= 20 && stretch > 500
+          ? setStretch((prev) => prev - 5)
+          : diff >= 50 && stretch < 1000
+          ? setStretch((prev) => prev + 5)
+          : null;
+        console.log(stretch);
+      };
+
+      const elapsed = timeStamp - deltaTimeTracker.current;
+      elapsed > 1000 / 60 &&
+        (adjustStretch(diff), (deltaTimeTracker.current = timeStamp));
+    },
+    [stretch, regulateStretch, getDiff]
+  );
+
+  const debounceStretch = useCallback(() => {
+    clearTimeout(debounceTimer.current);
+    debounceTimer.current = setTimeout(() => {
+      requestAnimationFrame(stretchTitle);
     }, 500);
-  };
-
-  const stretchTitle = () => {
-    if (!titleRef.current || !iconRef.current) return;
-    const titleRect = titleRef.current.getBoundingClientRect();
-    const iconRect = iconRef.current.getBoundingClientRect();
-    const distance = iconRect.left - titleRect.right;
-
-    distance >= 25 && stretch < 1000
-      ? (setStretch((current) => current + 1), setTimeout(stretchTitle, 100))
-      : distance <= 20
-      ? (setStretch((current) => current - 1), setTimeout(stretchTitle, 100))
-      : null;
-  };
+  }, [stretchTitle]);
 
   useEffect(() => {
-    stretchTitle();
-
+    requestAnimationFrame(stretchTitle);
     window.addEventListener("resize", debounceStretch);
 
     return () => {
       window.removeEventListener("resize", debounceStretch);
     };
-  });
+  }, [stretchTitle, debounceStretch]);
 
   return (
     <div
